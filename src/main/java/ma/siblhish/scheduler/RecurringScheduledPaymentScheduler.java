13ed109124b29d43d83171c102fb812debad7afa
Youssef Benlaghrissi <youssef.benlaghrissi.ext@cleva-solutions.com>
2026-02-22 02:03:04 +0000

    paiement test

--- src/main/java/ma/siblhish/scheduler/RecurringScheduledPaymentScheduler.java
+++ src/main/java/ma/siblhish/scheduler/RecurringScheduledPaymentScheduler.java
@@ -42,7 +42,7 @@ public class RecurringScheduledPaymentScheduler {
      * Créer les prochains paiements planifiés récurrents.
      * Exécuté tous les jours à 04:00
      */
-    @Scheduled(cron = "0 5 2 * * ?")
+    @Scheduled(cron = "0 10 2 * * ?")
     @Transactional
     public void createNextRecurringPayments() {
         createNextRecurringPaymentsInternal();
