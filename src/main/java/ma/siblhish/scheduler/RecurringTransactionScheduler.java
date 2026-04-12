49f03f52883dc1e68a277374ecc5cbbd3944e321
Youssef Benlaghrissi <youssef.benlaghrissi.ext@cleva-solutions.com>
2026-04-12 23:08:18 +0100

    im ready

--- src/main/java/ma/siblhish/scheduler/RecurringTransactionScheduler.java
+++ src/main/java/ma/siblhish/scheduler/RecurringTransactionScheduler.java
@@ -29,7 +29,7 @@ public class RecurringTransactionScheduler {
     private final IncomeRepository incomeRepository;
     private final NotificationService notificationService;
 
-    @Scheduled(cron = "0 35 05 * * ?")
+    @Scheduled(cron = "0 35 04 * * ?")
     @Transactional
     public void generateRecurringTransactions() {
         generateRecurringTransactionsForDate();
